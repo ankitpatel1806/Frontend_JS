7f72994eca910495f79f9bee4d4f222cdfdf33f2
Ankit Patel <yugap1806@gmail.com>
2025-03-01 13:42:49 +0530

    Ankit Patel Added Program to Display Weekday from Number

---
+++ Day01_JS_Basic/Switch_Case/weekDay.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function getWeekDay(number) {
+    switch (number) {
+        case 1:
+            console.log("Sunday");
+            break;
+        case 2:
+            console.log("Monday");
+            break;
+        case 3:
+            console.log("Tuesday");
+            break;
+        case 4:
+            console.log("Wednesday");
+            break;
+        case 5:
+            console.log("Thursday");
+            break;
+        case 6:
+            console.log("Friday");
+            break;
+        case 7:
+            console.log("Saturday");
+            break;
+        default:
+            console.log("Invalid input. Please enter a number between 1 and 7.");
+    }
+}
+
+rl.question('Enter a number (1-7) for a day of the week: ', (input) => {
+    getWeekDay(parseInt(input));
+    rl.close();
+});
