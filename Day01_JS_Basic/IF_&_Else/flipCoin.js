b0488858e8e365eccca2eef76101d6577f3ff809
Ankit Patel <yugap1806@gmail.com>
2025-03-01 13:35:36 +0530

    Ankit Patel Added Program to Simulate Coin Flip

---
+++ Day01_JS_Basic/IF_&_Else/flipCoin.js
@@ -0,0 +1,11 @@
+function flipCoin() {
+        let flip = Math.floor(Math.random() * 2);
+
+    if (flip === 0) {
+        console.log("Heads");
+    } else {
+        console.log("Tails");
+    }
+}
+
+flipCoin();
