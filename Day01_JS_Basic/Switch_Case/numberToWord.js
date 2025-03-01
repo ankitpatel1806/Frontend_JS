dcad1a3f227ca6ece9ad6da7a1d546a3146c47b6
Ankit Patel <yugap1806@gmail.com>
2025-03-01 13:41:51 +0530

    Ankit Patel Added Program to Convert Single Digit Number to Word Using Case

---
+++ Day01_JS_Basic/Switch_Case/numberToWord.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function numberToWord(number) {
+    switch (number) {
+        case 0:
+            console.log("Zero");
+            break;
+        case 1:
+            console.log("One");
+            break;
+        case 2:
+            console.log("Two");
+            break;
+        case 3:
+            console.log("Three");
+            break;
+        case 4:
+            console.log("Four");
+            break;
+        case 5:
+            console.log("Five");
+            break;
+        case 6:
+            console.log("Six");
+            break;
+        case 7:
+            console.log("Seven");
+            break;
+        case 8:
+            console.log("Eight");
+            break;
+        case 9:
+            console.log("Nine");
+            break;
+        default:
+            console.log("Invalid number, please enter a single digit.");
+    }
+}
+
+rl.question('Enter a single digit number: ', (input) => {
+    numberToWord(parseInt(input));
+    rl.close();
+});
